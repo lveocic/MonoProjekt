1edecdec7fa8b81cabcc31cd5b41122e1aeca8b0
DESKTOP-LB592VH\lveoc <lveocic@hotmail.com>
2022-06-19 20:16:13 +0200

    Project routes fix

---
+++ MonoProjekt.Service/Service/VehicleModelService.cs
@@ -0,0 +1,28 @@
+﻿using MonoProjekt.Service.Repository.Common;
+using MonoProjekt.Service.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProjekt.Service.Service
+{
+    public class VehicleModelService : IVehicleModelService
+    {
+        #region Constructors
+
+        public VehicleModelService(IVehicleModelRepository vehicleModelRepository)
+        {
+            VehicleModelRepository = vehicleModelRepository;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        private IVehicleModelRepository VehicleModelRepository { get; set; }
+
+        #endregion Properties
+    }
+}
